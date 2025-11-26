6ae84e454e588d63cbd9542869ffa536a2e7606b
Gustavo <Gustavo@DESKTOP-VEAA558>
2025-11-26 19:15:04 -0300

    layout do dashboard

--- ProjetoIntegradorSENAC/Dashboard/dashboard.Designer.cs
+++ ProjetoIntegradorSENAC/Dashboard/dashboard.Designer.cs
@@ -28,36 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnProdutos = new Button();
+            btnMeses = new Button();
+            btnPadrao = new Button();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            groupBox4 = new GroupBox();
+            groupBox5 = new GroupBox();
+            grafico1 = new OxyPlot.WindowsForms.PlotView();
+            groupBox6 = new GroupBox();
+            grafico2 = new OxyPlot.WindowsForms.PlotView();
+            comboBox1 = new ComboBox();
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            groupBox6.SuspendLayout();
             SuspendLayout();
+            // 
+            // btnProdutos
+            // 
+            btnProdutos.Location = new Point(1053, 53);
+            btnProdutos.Name = "btnProdutos";
+            btnProdutos.Size = new Size(142, 54);
+            btnProdutos.TabIndex = 0;
+            btnProdutos.Text = "Produtos";
+            btnProdutos.UseVisualStyleBackColor = true;
+            // 
+            // btnMeses
+            // 
+            btnMeses.Location = new Point(678, 53);
+            btnMeses.Name = "btnMeses";
+            btnMeses.Size = new Size(142, 54);
+            btnMeses.TabIndex = 1;
+            btnMeses.Text = "Meses";
+            btnMeses.UseVisualStyleBackColor = true;
+            // 
+            // btnPadrao
+            // 
+            btnPadrao.Location = new Point(323, 53);
+            btnPadrao.Name = "btnPadrao";
+            btnPadrao.Size = new Size(142, 54);
+            btnPadrao.TabIndex = 2;
+            btnPadrao.Text = "Padrão";
+            btnPadrao.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(77, 142);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 125);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label2);
+            groupBox2.Location = new Point(452, 142);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(250, 125);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label3);
+            groupBox3.Location = new Point(820, 142);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(250, 125);
+            groupBox3.TabIndex = 4;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label4);
+            groupBox4.Location = new Point(1196, 142);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(250, 125);
+            groupBox4.TabIndex = 4;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "groupBox4";
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(grafico1);
+            groupBox5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox5.Location = new Point(77, 324);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(625, 440);
+            groupBox5.TabIndex = 5;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "grafico1";
+            // 
+            // grafico1
+            // 
+            grafico1.Location = new Point(18, 26);
+            grafico1.Name = "grafico1";
+            grafico1.PanCursor = Cursors.Hand;
+            grafico1.Size = new Size(586, 391);
+            grafico1.TabIndex = 0;
+            grafico1.Text = "plotView1";
+            grafico1.ZoomHorizontalCursor = Cursors.SizeWE;
+            grafico1.ZoomRectangleCursor = Cursors.SizeNWSE;
+            grafico1.ZoomVerticalCursor = Cursors.SizeNS;
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(grafico2);
+            groupBox6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox6.Location = new Point(820, 324);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(626, 440);
+            groupBox6.TabIndex = 6;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "grafico2";
+            // 
+            // grafico2
+            // 
+            grafico2.Location = new Point(19, 26);
+            grafico2.Name = "grafico2";
+            grafico2.PanCursor = Cursors.Hand;
+            grafico2.Size = new Size(586, 391);
+            grafico2.TabIndex = 0;
+            grafico2.Text = "plotView2";
+            grafico2.ZoomHorizontalCursor = Cursors.SizeWE;
+            grafico2.ZoomRectangleCursor = Cursors.SizeNWSE;
+            grafico2.ZoomVerticalCursor = Cursors.SizeNS;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "1 Mês", "6 Meses", "12 Meses" });
+            comboBox1.Location = new Point(77, 53);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(141, 28);
+            comboBox1.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Franklin Gothic Medium", 99.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(233, 9);
+            label1.Location = new Point(91, 56);
             label1.Name = "label1";
-            label1.Size = new Size(817, 168);
-            label1.TabIndex = 1;
-            label1.Text = "DASHBOARD";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(91, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 0;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(98, 56);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 0;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(113, 56);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 0;
+            label4.Text = "label4";
             // 
             // dashboard
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(45, 45, 60);
-            ClientSize = new Size(1326, 686);
-            Controls.Add(label1);
+            ClientSize = new Size(1515, 915);
+            Controls.Add(comboBox1);
+            Controls.Add(groupBox6);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox1);
+            Controls.Add(btnPadrao);
+            Controls.Add(btnMeses);
+            Controls.Add(btnProdutos);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "dashboard";
             Text = "dashboard";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox6.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
+        private Button btnProdutos;
+        private Button btnMeses;
+        private Button btnPadrao;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private GroupBox groupBox5;
+        private GroupBox groupBox6;
+        private OxyPlot.WindowsForms.PlotView grafico1;
+        private OxyPlot.WindowsForms.PlotView grafico2;
+        private ComboBox comboBox1;
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
